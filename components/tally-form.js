ab797c36c0e0f90a601c09015428da59ae78bd88
Arth K. Gajjar <iarthstar@gmail.com>
2023-05-21 05:26:55 +0530

    Fixes : Tally brand hiding

--- components/tally-form.js
+++ components/tally-form.js
@@ -2,26 +2,44 @@
 
 import Script from "next/script";
 import { useEffect } from "react";
+import { ZStack } from "@/components/shared";
 
 export const TallyForm = () => {
   useEffect(() => {
     window?.Tally?.loadEmbeds?.();
   }, []);
-  
+
   return (
-    <>
-      <Script src="https://tally.so/widgets/embed.js" />
-      <iframe
-        className="-ml-2"
-        data-tally-src="https://tally.so/embed/wvrYp4?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
-        loading="lazy"
-        width="100%"
-        height="292"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
-        title="Contact Us"
-      />
-    </>
+    <ZStack
+      list={[
+        {
+          zIndex: "z-10",
+          view: (
+            <>
+              <Script src="https://tally.so/widgets/embed.js" />
+              <iframe
+                className="-ml-2"
+                data-tally-src="https://tally.so/embed/wvrYp4?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+                loading="lazy"
+                width="100%"
+                height="292"
+                frameborder="0"
+                marginheight="0"
+                marginwidth="0"
+                title="Contact Us"
+              />
+            </>
+          ),
+        },
+        {
+          zIndex: "z-20 pointer-events-none",
+          view: (
+            <div className="h-full w-full flex items-end justify-end max-[432px]:justify-start">
+              <div className="pointer-events-auto h-16 w-40 bg-secondary" />
+            </div>
+          ),
+        },
+      ]}
+    />
   );
 };
